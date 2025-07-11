5122e115b8b4e0f27c9b6a3551ceada2c49206fc
Randy-M <142512902+buma2510@users.noreply.github.com>
2025-07-11 13:41:17 +0100

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "bumy-fintechapp-state-bucket"
     key            = "ecs/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "eu-west-2"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
