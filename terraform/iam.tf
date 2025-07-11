a5f9c8b9c9b28f2fc2d08564c992c9bdb1c2736a
Randy-M <142512902+buma2510@users.noreply.github.com>
2025-07-11 13:20:30 +0100

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -5,9 +5,14 @@ resource "aws_iam_role" "ecs_task_execution" {
 data "aws_iam_policy_document" "ecs_assume" {
   statement {
     actions = ["sts:AssumeRole"]
-    principals { type = "Service"; identifiers = ["ecs-tasks.amazonaws.com"] }
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
   }
 }
+
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
   role       = aws_iam_role.ecs_task_execution.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
